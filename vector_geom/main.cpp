ac41c3c4e65ebf92129ffe548d0bc9dfdd46eae3
ahbpp <ahbpp@rambler.ru>
2017-02-17 02:27:22 +0300

    Initial Commit

---
+++ vector_geom/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vector_geom
+//
+//  Created by Alexey Karpov on 17.02.17.
+//  Copyright © 2017 Alexey Karpov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
